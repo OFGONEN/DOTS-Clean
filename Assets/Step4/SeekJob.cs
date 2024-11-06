2f0b7de3b59033522da17382e6b88dc056d19c15
ofgonen <ofgonen@gmail.com>
2024-11-06 18:32:57 +0300

    feat: implement step 4
    - Implement Data Locality with Multi-Threading

--- Assets/Step4/SeekJob.cs
+++ Assets/Step4/SeekJob.cs
@@ -1,24 +1,26 @@
-using Unity.Burst;
+using Unity.Jobs;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine.Jobs;
+using Unity.Burst;
 
-namespace Jobs_Demo.Step2
+
+namespace Jobs_Demo.Step4
 {
     [BurstCompile]
-    public struct SeekJob : IJobParallelForTransform
+    public struct SeekJob : IJobParallelFor
     {
         [ReadOnly] public float SeekRadiusSq;
         [ReadOnly] public float3 CurrentPosition;
+        [ReadOnly] public NativeArray<float3> TargetPositions;
 
         [NativeDisableParallelForRestriction] public NativeArray<int> TargetIndices;
 
 
         [BurstCompile]
-        public void Execute(int index, TransformAccess transform)
+        public void Execute(int index)
         {
-            var targetPosition = transform.position;
-            var distance = math.distancesq(targetPosition, CurrentPosition);
+            var distance = math.distancesq(CurrentPosition, TargetPositions[index]);
 
             if (distance < SeekRadiusSq)
             {
---
+++ Assets/Step4/Spawner.cs
@@ -0,0 +1,49 @@
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Jobs_Demo.Step4
+{
+    public class Spawner : MonoBehaviour
+    {
+        public static Transform[] TargetTransforms;
+        public static Renderer[] TargetRenderers;
+
+        public static NativeArray<float3> TargetPositions;
+
+        public int TargetCount;
+        public GameObject TargetPrefab;
+
+        public float SpawnRadius;
+
+        private void Awake()
+        {
+            UnityEngine.Random.InitState(52);
+
+            TargetTransforms = new Transform[TargetCount];
+            TargetRenderers = new Renderer[TargetCount];
+            TargetPositions = new NativeArray<float3>(TargetCount, Allocator.Persistent);
+
+            for (int i = 0; i < TargetCount; i++)
+            {
+                var spawnPosition = UnityEngine.Random.insideUnitSphere * SpawnRadius;
+                var target = Instantiate(TargetPrefab, spawnPosition, Quaternion.identity);
+
+                TargetTransforms[i] = target.transform;
+                TargetRenderers[i] = target.GetComponent<Renderer>();
+                TargetPositions[i] = spawnPosition;
+            }
+        }
+
+        private void OnDestroy() 
+        {
+            TargetPositions.Dispose();
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, SpawnRadius);
+        }
+    }
+}
